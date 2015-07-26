2d876386c0c453ab6fcb043aa7bc1a330d98aea3
vaagrawa <vaibhavajay@gmail.com>
2015-07-26 14:38:39 -0700

    Wrote the makeCacheMatrix Function

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache_matrix <- NULL
+  #Empty the Cache
+  #Create getter and setters for the matirx
+  setM <- function(y){
+    x<<-y
+    cache_matrix<<-NULL #flush the cache since a new matrix is set.
+    
+  }
+  getM<-function()x # return the matrix
+  
+  setCache<-function(mat){
+    cache_matrix<<-mat         # store the value passed in mat in the cache             
+  }
+  getCache<-function() cache_matrix #return the values in the cache
+  list(setMatrix = setM, getMatrix = getM, cacheInverse = setcache, getInverse = getCache)
+  
 }
 
 
